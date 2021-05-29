c4f4d519b197837f60f58d1f975af0d575aaf565
myrthe-vt <72264161+myrthe-vt@users.noreply.github.com>
2021-05-29 22:03:37 +0200

    update

---
+++ keys.py
@@ -0,0 +1,6 @@
+keys = dict(
+	consumer_key =          "juLJBETl97rNVgINosn0nYd16",
+	consumer_secret =       "exqXWwJofGCjbPdDAkdVLXwuCM0563N7XhtEGH5kb71t6emk8r",
+	access_token =          "1398673854889959424-CJVc4moAH3LHST4gpqRfC5m0w7f1O5",
+	access_token_secret =   "OoKp9fJ4JkbS2lDSt7RHAtwXlq4SqxIY21EmnclJyEbLf",
+)
